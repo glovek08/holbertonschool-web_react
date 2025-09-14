ac2e2ad492d3d58fa8a97f458543afb697b40616
Gabriel Barn <gbrielbbarn@gmail.com>
2025-09-14 18:32:35 -0300

    feat: student class defined through interface and constructor

--- task_1/js/main.ts
+++ task_1/js/main.ts
@@ -38,8 +38,10 @@ console.log(teacher3);
 // lastName: "Doe"
 // location: "London"
 
-// ******************************   Task 2 **********************************
-// Write an interface named Directors that extends Teacher. It requires an attribute named numberOfReports(number)
+// ****************************** TASK 2 **********************************
+
+// Write an interface named Directors that extends Teacher.
+// It requires an attribute named numberOfReports(number)
 
 interface Directors extends Teacher {
   numberOfReports: number
@@ -53,7 +55,7 @@ const director1: Directors = {
   numberOfReports: 17,
 };
 
-console.log("TASK 2:");
+console.log("TASK 2");
 
 console.log(director1);
 
@@ -64,3 +66,83 @@ console.log(director1);
 // lastName: "Doe"
 // location: "London"
 // numberOfReports: 17
+
+
+// ************************** TASK 3 ********************************
+
+// Write a function printTeacher:
+
+//     It accepts two arguments firstName and lastName
+//     It returns the first letter of the firstName and the full lastName
+//     Example: printTeacher("John", "Doe") -> J. Doe
+
+// Write an interface for the function named printTeacherFunction.
+
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+const printTeacher: printTeacherFunction = (firstName, lastName) => {
+  return `${firstName[0]}. ${lastName}`;
+};
+
+console.log(`TASK 3`);
+
+console.log(printTeacher(director1.firstName, director1.lastName));
+
+// *************************** TASK 4 *********************************
+
+// Write a Class named StudentClass:
+
+//     The constructor accepts firstName(string) and lastName(string) arguments
+//     The class has a method named workOnHomework that return the string Currently working
+//     The class has a method named displayName. It returns the firstName of the student
+//     The constructor of the class should be described through an Interface
+//     The class should be described through an Interface
+
+// Requirements:
+
+//     You can reuse the Webpack configuration from the previous exercise to compile the code.
+//     When running npm run build, no TypeScript error should be displayed.
+//     Every variable should use TypeScript when possible.
+
+interface InterfaceStudent {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+interface ConstructorStudent {
+  new (
+    firstName: string,
+    lastName: string,
+  ): InterfaceStudent
+}
+
+class StudentClass implements InterfaceStudent {
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+  workOnHomework(): string {
+    return 'Currently Working'
+  }
+  displayName(): string {
+    return this.firstName;
+  }
+}
+
+const student = new StudentClass("Pepe", "Sanchez");
+// try {
+//   const student1 = new StudentClass("Pepe", 123);
+// } catch (error) {
+//   console.log(error)
+// }
+// const student2 = new StudentClass("Pepe", 23);
+
+console.log("TASK 4")
+console.log(student);
+
+
